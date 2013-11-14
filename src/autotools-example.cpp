9f226020930e8f0f13eeec01c314b9b203408a55
QING Pei <edwardtoday@gmail.com>
2013-11-14 16:49:17 +0800

    initial commit

---
+++ src/autotools-example.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : autotools-example.cpp
+ Author      : qingpei
+ Version     :
+ Copyright   : 
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World!!!" << endl; /* prints Hello World!!! */
+	return 0;
+}
